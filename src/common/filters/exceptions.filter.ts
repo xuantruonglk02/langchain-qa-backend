c8d888fc45c0c6f7d488f3cdebead3964d3e65a4
truongpx <xuantruonglk02@gmail.com>
2023-07-20 09:28:03 +0700

    update exception filter

--- src/common/filters/exceptions.filter.ts
+++ src/common/filters/exceptions.filter.ts
@@ -1,4 +1,9 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import {
+    ArgumentsHost,
+    Catch,
+    HttpException,
+    HttpStatus,
+} from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Response } from 'express';
 
@@ -12,15 +17,25 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
 
-        const apiResponse = exception.getResponse() as any;
-        const status = exception.getStatus();
-        const parsedResponse = {
-            code: exception.getStatus(),
-            name: exception.name,
-            message: exception.message,
-            errors: apiResponse?.error || [],
-        };
+        try {
+            const apiResponse = exception.getResponse() as any;
+            const status = exception.getStatus();
 
-        return response.status(status).json(parsedResponse);
+            const parsedResponse = {
+                code: exception.getStatus(),
+                name: exception.name,
+                message: exception.message,
+                errors: apiResponse?.error || [],
+            };
+
+            return response.status(status).json(parsedResponse);
+        } catch (error) {
+            return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                code: HttpStatus.INTERNAL_SERVER_ERROR,
+                name: 'Internal Server Error',
+                message: exception.message,
+                error: exception,
+            });
+        }
     }
 }
