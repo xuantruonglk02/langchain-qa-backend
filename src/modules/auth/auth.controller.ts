7e54d1bea71fc811104c3ef3d7247d07f29e7ed8
truongpx <xuantruonglk02@gmail.com>
2023-07-21 14:21:48 +0700

    update login with google

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,25 @@
+import { ConfigKey } from '@/common/configs/config-keys';
 import {
     Controller,
     Get,
+    HttpStatus,
     InternalServerErrorException,
     Logger,
     Req,
+    Res,
+    UnauthorizedException,
     UseGuards,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
+import url from 'url';
 import { AuthService } from './services/auth.service';
 
 @Controller('/auth')
 export class AuthController {
     constructor(
+        private readonly configService: ConfigService,
         private readonly logger: Logger,
         private readonly authService: AuthService,
     ) {}
@@ -23,9 +31,27 @@ export class AuthController {
 
     @Get('/google/callback')
     @UseGuards(AuthGuard('google'))
-    googleAuthCallback(@Req() req: any) {
+    googleAuthCallback(@Req() req: any, @Res() res: Response) {
         try {
-            return this.authService.googleLogin(req);
+            const loginResponse = this.authService.googleLogin(req);
+            if (!loginResponse) {
+                throw new UnauthorizedException();
+            }
+
+            return res.status(HttpStatus.OK).redirect(
+                url.format({
+                    pathname: this.configService.get(
+                        ConfigKey.GOOGLE_REDIRECT_URL,
+                    ),
+                    query: {
+                        ...loginResponse.user,
+                        ...loginResponse.accessToken,
+                        _id: loginResponse.user._id.toString(),
+                        expiresIn:
+                            loginResponse.accessToken.expiresIn.toUTCString(),
+                    },
+                }),
+            );
         } catch (error) {
             this.logger.error(
                 'In googleAuthCallback()',
--- src/plugins/moment.ts
+++ src/plugins/moment.ts
@@ -4,13 +4,21 @@ import 'moment-timezone';
 declare module 'moment' {
     interface Moment {
         /**
-         * format YYYY-MM-DD HH:mm:ss
+         * format YYYY-MM-DDTHH:mm:ss
+         * @return string
+         */
+        fmFullTimeString(): string;
+        /**
+         * format YYYY-MM-DDTHH:mm:ss+00:00
          * @return string
          */
         fmFullTimeStringWithTimezone(): string;
     }
 }
 
+moment.fn.fmFullTimeString = function (): string {
+    return this.tz('UTC').format('YYYY-MM-DDTHH:mm:ss');
+};
 moment.fn.fmFullTimeStringWithTimezone = function (): string {
     return this.tz('UTC').format('YYYY-MM-DDTHH:mm:ss+00:00');
 };
